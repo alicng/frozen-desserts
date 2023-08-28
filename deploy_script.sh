f6bd79547f82f3b13cb2b9acb955be7ac0af720a
alicng <mypublicemail911@gmail.com>
2023-08-27 19:26:54 -0500

    updated deploy.yaml

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+# Set the SSH directory
+SSH_DIR="$HOME/.ssh"
+SSH_KEY=~/.ssh/deploy_key
+
+# Ensure that the private key is stored in the correct file
+echo "$EC2_SSH_PRIVATE_KEY" > "$SSH_DIR/deploy_key"
+chmod 600 "$SSH_DIR/deploy_key"
+
 # Replace these variables with your actual values
 EC2_USER="$EC2_USER"
 EC2_HOST="$EC2_INSTANCE_IP"
 APP_DIR="/var/www/my_rails_app"  # Change this to the actual directory path on the server
 
-# Load the SSH private key content from GitHub Secrets
-echo "$EC2_SSH_PRIVATE_KEY" > ~/.ssh/deploy_key
-chmod 600 ~/.ssh/deploy_key
-
 # Copy your compiled code to the server
 rsync -avz --delete --exclude=".git" ./ $EC2_USER@$EC2_HOST:$APP_DIR
 
 # SSH into the server and perform further deployment steps
-ssh -i ~/.ssh/deploy_key $EC2_USER@$EC2_HOST << EOF
+ssh -i "$SSH_DIR/deploy_key" $EC2_USER@$EC2_HOST << EOF
   cd $APP_DIR
   
   # Example: Stop the old Puma process
@@ -32,3 +36,4 @@ ssh -i ~/.ssh/deploy_key $EC2_USER@$EC2_HOST << EOF
 EOF
 
 echo "Deployment complete"
+
